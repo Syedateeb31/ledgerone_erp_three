a34a20ab45ac41e0a61178a934f103c4707de332
syed muhammad ateeb <ateebmuhammad685@gmail.com>
2026-03-12 20:22:55 +0800

    Revert "Update index.php"
    This reverts commit c5dfdad3deeaec6f424127f0badd7b2878a51a4b.

--- client/pages/manuacturing/bill_of_material/index.php
+++ client/pages/manuacturing/bill_of_material/index.php
@@ -19,7 +19,7 @@ if (!$user_id) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Testing</title>
+    <title>Bill of Materials (BOM) entry</title>
     <!-- Font & icons (Inter, plus simple line icons for +/- actions) -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
