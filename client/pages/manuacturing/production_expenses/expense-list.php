fc16241ac991acec23928194013025c6e30197c4
InnovaTech-Official <innova.tech213@gmail.com>
2026-04-11 22:58:01 +0500

    Update expense-list.php

--- client/pages/manuacturing/production_expenses/expense-list.php
+++ client/pages/manuacturing/production_expenses/expense-list.php
@@ -27,7 +27,7 @@ if (!$user_id) {
         rel="stylesheet">
     <!-- Font Awesome 6 -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-    <link rel="stylesheet" href="../../../assets/css/manufacturing/production_expenses/expense-list.css">
+    <link rel="stylesheet" href="../../../assets/css/manufacturing/production_expenses/expense-list.css?v=<?php echo time(); ?>">
 </head>
 
 <body>
